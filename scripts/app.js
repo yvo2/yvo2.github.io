38ae24ce8025265b92d3251d24f13efd841f1831
yvo2 <yvo.a.hofer@bluewin.ch>
2022-11-15 17:18:58 +0100

    introduce CV

--- scripts/app.js
+++ scripts/app.js
@@ -16,22 +16,17 @@ const points = createPoints();
 (function animate() {
     path.setAttribute("d", spline(points, 1, true));
 
-    // for every point
     for (let i = 0; i < points.length; i++) {
         const point = points[i];
 
-        // return a pseudo random value between -1 / 1 based on this point's current x, y positions in "time"
         const nX = noise(point.noiseOffsetX, point.noiseOffsetX);
         const nY = noise(point.noiseOffsetY, point.noiseOffsetY);
-        // map this noise value to a new value, somewhere between its original location -20 and its original location + 20
         const x = map(nX, -1, 1, point.originX - 20, point.originX + 20);
         const y = map(nY, -1, 1, point.originY - 20, point.originY + 20);
 
-        // update the point's current coordinates
         point.x = x;
         point.y = y;
 
-        // progress the point's x, y values through "time"
         point.noiseOffsetX += noiseStep;
         point.noiseOffsetY += noiseStep;
     }
@@ -57,28 +52,21 @@ function noise(x, y) {
 
 function createPoints() {
     const points = [];
-    // how many points do we need
     const numPoints = 6;
-    // used to equally space each point around the circle
     const angleStep = (Math.PI * 2) / numPoints;
-    // the radius of the circle
     const rad = 75;
 
     for (let i = 1; i <= numPoints; i++) {
-        // x & y coordinates of the current point
         const theta = i * angleStep;
 
         const x = 100 + Math.cos(theta) * rad;
         const y = 100 + Math.sin(theta) * rad;
 
-        // store the point's position
         points.push({
             x: x,
             y: y,
-            // we need to keep a reference to the point's original point for when we modulate the values later
             originX: x,
             originY: y,
-            // more on this in a moment!
             noiseOffsetX: Math.random() * 1000,
             noiseOffsetY: Math.random() * 1000
         });
